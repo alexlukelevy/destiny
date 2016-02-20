c1adfafbb7447016a0bf0f233f161f79430c9d83
alexlukelevy <alex.levy@hotmail.co.uk>
2016-02-20 14:59:45 +0000

    Added ability to look up membership summary with username

--- src/DestinyService.java
+++ src/DestinyService.java
@@ -4,4 +4,6 @@ public interface DestinyService {
 
     String getAccountSummary(int membershipTypeId, long membershipId) throws IOException;
 
+    long getMembershipId(int membershipTypeId, String username) throws IOException;
+
 }
